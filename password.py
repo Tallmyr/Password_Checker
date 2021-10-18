b76403bb26f0b51278c277957f42e0a25d5a27fc
Simon Tallmyr <simon@tallmyr.se>
2021-10-18 20:42:56 +0100

    Added separate Hash function & changed out to int

--- password.py
+++ password.py
@@ -9,30 +9,35 @@ def Main():
     loop = True
     print("Welcome to Password Checker")
     while loop:
-        password = hashlib.sha1(
-            getpass("Enter Password to test: ").encode()).hexdigest().upper()
-        result = checkpassword(password)
+        hash = hash_password(getpass("Enter Password to test: "))
+        result = checkpassword(hash)
         print(result)
         loop = strtobool(
-            input("Do you wish to test another password? "
-                  "[Yes, y] / [No, n] ").lower())
+            input(
+                "Do you wish to test another password? " "[Yes, y] / [No, n] "
+            ).lower()
+        )
 
 
-def checkpassword(password):
+def hash_password(password: str):
+    return hashlib.sha1(password.encode()).hexdigest().upper()
+
+
+def checkpassword(hash: str):
     # Take password input, hash, convert to text and make upper case
-    front = password[:5]  # First 5 letters
-    back = password[5:]  # rest of string
+    front = hash[:5]  # First 5 letters
+    back = hash[5:]  # rest of string
 
     # request the list of hashes
-    pwlist = requests.get('https://api.pwnedpasswords.com/range/' + front)
+    pwlist = requests.get("https://api.pwnedpasswords.com/range/" + front)
     regex = r"{}:([0-9]*)".format(back)  # build regex
     result = re.search(regex, pwlist.text)
 
     if result is not None:  # If nothing found result is None
-        return("Found your password {} times".format(
-            result.group(1)))  # Group 1 for the number
+        return result.group(1)
     else:
-        return("Your password was not found.")
+        return None
+
 
 
 if __name__ == "__main__":
