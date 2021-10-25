eecb44c3847071cc33d726a3a9602ac7fbcd9147
Simon Tallmyr <simon@tallmyr.se>
2021-10-26 00:51:37 +0100

    Setup Push Workflow Fixes #12

--- noxfile.py
+++
@@ -1,6 +0,0 @@
-import nox@nox.session(python=["3.7", "3.8"])
-def lint(session):
-    session.install("poetry")
-    session.run("poetry", "install")
-    session.run("black", "--check", ".")
-    session.run("flake8", ".")@nox.session
